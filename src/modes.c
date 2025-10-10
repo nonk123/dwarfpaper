adc1b8b65371229c64c4a5a581745e30812b58cc
nonk123 <me@nonk.dev>
2025-10-10 10:11:33 +0300

    Lay down the foundations for a forest background

--- src/modes.c
+++ src/modes.c
@@ -1,12 +1,13 @@
 #include <SDL3/SDL_stdinc.h>
 
 #include "modes.h"
-
 #include "modes/cave.h"
+#include "modes/forest.h"
 #include "modes/pipes.h"
 
 const ModeTable modes[] = {
-	{"pipes", NULL, update_pipes},
-	{"cave",  NULL, update_cave },
-	{NULL,    NULL, NULL        },
+	{"forest", NULL, update_forest},
+	{"cave",   NULL, update_cave  },
+	{"pipes",  NULL, update_pipes },
+	{NULL,     NULL, NULL         },
 };
---
+++ src/modes/forest.c
@@ -0,0 +1,125 @@
+#include <SDL3/SDL_stdinc.h>
+
+#include "colors.h"
+#include "noise.h"
+#include "screen.h"
+
+#include "modes/forest.h"
+
+typedef struct {
+	uint8_t initialized;
+} State;
+
+#define SKY (C_BLACK)
+
+#define SEED_PROB (8)
+#define SEED_GROW_PROB (3)
+static const uint8_t seed[] = {'.', ',', ':', '%', '$'}, seed_max = sizeof(seed);
+
+static int flip(int y) {
+	return screen_rows() - 1 - y;
+}
+
+static void clear_spot(int x, int y) {
+	y = flip(y);
+	cell_at(x, y)->bg = cell_at(x, y)->fg = SKY;
+	cell_at(x, y)->chr = ' ';
+}
+
+static void place_rock(int x, int y) {
+	y = flip(y);
+	cell_at(x, y)->bg = cell_at(x, y)->fg = C_WHITE;
+	cell_at(x, y)->chr = '#';
+}
+
+static void place_dirt(int x, int y) {
+	y = flip(y);
+	cell_at(x, y)->bg = cell_at(x, y)->fg = C_YELLOW;
+	cell_at(x, y)->chr = '#';
+}
+
+static void place_grass(int x, int y) {
+	y = flip(y);
+	cell_at(x, y)->bg = cell_at(x, y)->fg = C_GREEN;
+	cell_at(x, y)->chr = '#';
+}
+
+static void place_seed(int x, int y, int stage) {
+	y = flip(y);
+	cell_at(x, y)->bg = SKY;
+	cell_at(x, y)->fg = C_YELLOW;
+	cell_at(x, y)->chr = stage;
+}
+
+static void drop_seed(int x) {
+	place_seed(x, screen_rows() - 1, seed[0]);
+}
+
+static int height_at(int ix) {
+	const int sloping = 12, breadth = 64;
+	const float x = ((float)ix) / ((float)breadth);
+	return (int)(sloping * noise_at(x, 0.f));
+}
+
+static void generate(State* this) {
+	this->initialized = 1;
+	const int baseline = screen_rows() / 6, rockbed = 8;
+	for (int ix = 0; ix < screen_cols(); ix++) {
+		int iy = 0, height = height_at(ix), prev_height = height_at(ix - 1);
+		for (; iy < baseline + height - rockbed; iy++)
+			place_rock(ix, iy);
+		for (; iy < baseline + height; iy++)
+			place_dirt(ix, iy);
+		if (prev_height < height)
+			place_grass(ix, baseline + prev_height);
+		else if (prev_height > height)
+			place_grass(ix - 1, baseline + prev_height - 1);
+		place_grass(ix, iy++);
+		for (; iy < screen_rows(); iy++)
+			clear_spot(ix, iy);
+		if (SDL_rand(100) < SEED_PROB)
+			drop_seed(ix);
+	}
+}
+
+static int is_seed(int x, int y) {
+	for (int i = 0; i < seed_max; i++) {
+		if (cell_at(x, flip(y))->chr == seed[i])
+			return 1;
+	}
+	return 0;
+}
+
+static void move_seed(int x, int y) {
+	if (cell_at(x, flip(y - 1))->chr != ' ')
+		return;
+	place_seed(x, y - 1, cell_at(x, flip(y))->chr);
+	clear_spot(x, y);
+}
+
+static void grow_seed(int x, int y) {
+	y = flip(y);
+	for (int i = 0; i < seed_max - 1; i++)
+		if (seed[i] == cell_at(x, y)->chr) {
+			cell_at(x, y)->chr = seed[i + 1];
+			return;
+		}
+	cell_at(x, y)->chr = '#';
+	cell_at(x, y)->fg = cell_at(x, y)->bg = C_YELLOW;
+}
+
+void update_forest(void* _this) {
+	State* this = _this;
+	if (!this->initialized) {
+		generate(this);
+		return;
+	}
+	for (int x = 0; x < screen_cols(); x++)
+		for (int y = 0; y < screen_rows(); y++)
+			if (is_seed(x, y))
+				move_seed(x, y);
+	for (int x = 0; x < screen_cols(); x++)
+		for (int y = 0; y < screen_rows(); y++)
+			if (is_seed(x, y) && SDL_rand(100) < SEED_GROW_PROB)
+				grow_seed(x, y);
+}
